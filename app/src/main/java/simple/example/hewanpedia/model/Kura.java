00598b3e7feed2d96ee4ace5857d43564b5c2400
milikromy <milikromy@gmail.com>
2020-11-25 09:05:12 +0700

    mil ikrom yani

---
+++ app/src/main/java/simple/example/hewanpedia/model/Kura.java
@@ -0,0 +1,10 @@
+package simple.example.hewanpedia.model;
+
+public class Kura extends Hewan{
+
+    public Kura(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kura",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
